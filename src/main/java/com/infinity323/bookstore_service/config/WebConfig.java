4402e9404707143fe9997eedc2a392259ca876ff
Infinity323 <paul_in_plano@hotmail.com>
2024-08-03 09:01:44 -0500

    add interceptor

---
+++ src/main/java/com/infinity323/bookstore_service/config/WebConfig.java
@@ -0,0 +1,19 @@
+package com.infinity323.bookstore_service.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.infinity323.bookstore_service.interceptor.PerformanceInterceptor;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new PerformanceInterceptor());
+    }
+
+}
---
+++ src/main/java/com/infinity323/bookstore_service/interceptor/PerformanceInterceptor.java
@@ -0,0 +1,27 @@
+package com.infinity323.bookstore_service.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class PerformanceInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+        request.setAttribute("start", System.currentTimeMillis());
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+            ModelAndView modelAndView) {
+        Long start = (Long) request.getAttribute("start");
+        log.info("Completed request for [{}] in {} ms", request.getRequestURI(), System.currentTimeMillis() - start);
+    }
+
+}
