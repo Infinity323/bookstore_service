b570e1c6b2b38e6829c1ce533e455e919eecbe2f
Infinity323 <paul_in_plano@hotmail.com>
2024-08-03 11:13:49 -0500

    remove errors from swagger config

--- src/main/java/com/infinity323/bookstore_service/config/SwaggerConfig.java
+++ src/main/java/com/infinity323/bookstore_service/config/SwaggerConfig.java
@@ -2,7 +2,13 @@ package com.infinity323.bookstore_service.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
 public class SwaggerConfig {
@@ -10,6 +16,15 @@ public class SwaggerConfig {
     @Bean
     public InternalResourceViewResolver defaultViewResolver() {
         return new InternalResourceViewResolver();
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
+                .paths(PathSelectors.any())
+                .build();
     }
 
 }
