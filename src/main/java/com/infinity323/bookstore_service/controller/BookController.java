471983ae99fe8394db1c9c3cf3ed5353c7678f34
Infinity323 <paul_in_plano@hotmail.com>
2024-08-05 19:33:53 -0500

    delete try/catch

--- src/main/java/com/infinity323/bookstore_service/controller/BookController.java
+++ src/main/java/com/infinity323/bookstore_service/controller/BookController.java
@@ -41,16 +41,9 @@ public class BookController {
     @ApiOperation(value = "Get Books", notes = "Endpoint to get books from database.")
     public ResponseEntity<ResponseDto> getBooks(@RequestParam(required = false) String title) {
         ResponseDto responseDto = new ResponseDto();
-        try {
-            responseDto.setData(bookService.getBooks(title));
-            responseDto.setStatusCode(HttpStatus.OK);
-            return new ResponseEntity<>(responseDto, HttpStatus.OK);
-        } catch (Exception e) {
-            log.error("Exception occurred while getting books with params [{}]", title, e);
-            responseDto.setMessage(e.getMessage());
-            responseDto.setStatusCode(HttpStatus.INTERNAL_SERVER_ERROR);
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+        responseDto.setData(bookService.getBooks(title));
+        responseDto.setStatusCode(HttpStatus.OK);
+        return new ResponseEntity<>(responseDto, HttpStatus.OK);
     }
 
     /**
@@ -63,18 +56,11 @@ public class BookController {
     @ApiOperation(value = "Get Book by OpenLibrary Key", notes = "Endpoint to get book from database by OpenLibrary key.")
     public ResponseEntity<ResponseDto> getBookByOlKey(@PathVariable String olKey) {
         ResponseDto responseDto = new ResponseDto();
-        try {
-            Book book = bookService.getBookByOlKey(olKey);
-            HttpStatus status = Objects.nonNull(book) ? HttpStatus.OK : HttpStatus.NOT_FOUND;
-            responseDto.setData(book);
-            responseDto.setStatusCode(status);
-            return new ResponseEntity<>(responseDto, status);
-        } catch (Exception e) {
-            log.error("Exception occurred while getting book by OpenLibrary key \"{}\"", olKey, e);
-            responseDto.setMessage(e.getMessage());
-            responseDto.setStatusCode(HttpStatus.INTERNAL_SERVER_ERROR);
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+        Book book = bookService.getBookByOlKey(olKey);
+        HttpStatus status = Objects.nonNull(book) ? HttpStatus.OK : HttpStatus.NOT_FOUND;
+        responseDto.setData(book);
+        responseDto.setStatusCode(status);
+        return new ResponseEntity<>(responseDto, status);
     }
 
     /**
@@ -88,16 +74,9 @@ public class BookController {
     @ApiOperation(value = "Sync Books with OpenLibrary", notes = "Endpoint to sync database with OpenLibrary book search results by title.")
     public ResponseEntity<ResponseDto> synchronizeBooks(@RequestParam String title) {
         ResponseDto responseDto = new ResponseDto();
-        try {
-            responseDto.setData(bookService.synchronizeBooks(title));
-            responseDto.setStatusCode(HttpStatus.OK);
-            return new ResponseEntity<>(responseDto, HttpStatus.OK);
-        } catch (Exception e) {
-            log.error("Exception occurred while synchronizing database", e);
-            responseDto.setMessage(e.getMessage());
-            responseDto.setStatusCode(HttpStatus.INTERNAL_SERVER_ERROR);
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+        responseDto.setData(bookService.synchronizeBooks(title));
+        responseDto.setStatusCode(HttpStatus.OK);
+        return new ResponseEntity<>(responseDto, HttpStatus.OK);
     }
 
     /**
@@ -110,16 +89,9 @@ public class BookController {
     @ApiOperation(value = "Delete Book by OpenLibrary Key", notes = "Endpoint to delete book from database by OpenLibrary key.")
     public ResponseEntity<ResponseDto> deleteBookByOlKey(@PathVariable String olKey) {
         ResponseDto responseDto = new ResponseDto();
-        try {
-            responseDto.setData(bookService.deleteBookByOlKey(olKey));
-            responseDto.setStatusCode(HttpStatus.OK);
-            return new ResponseEntity<>(responseDto, HttpStatus.OK);
-        } catch (Exception e) {
-            log.error("Exception occurred while deleting book by OpenLibrary key \"{}\"", olKey, e);
-            responseDto.setMessage(e.getMessage());
-            responseDto.setStatusCode(HttpStatus.INTERNAL_SERVER_ERROR);
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+        responseDto.setData(bookService.deleteBookByOlKey(olKey));
+        responseDto.setStatusCode(HttpStatus.OK);
+        return new ResponseEntity<>(responseDto, HttpStatus.OK);
     }
 
 }
